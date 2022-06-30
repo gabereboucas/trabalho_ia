ab7f8795f0ac3854092e1c226630d48edebc1ff9
Gabriel Rebouças <gabrielrbsr@gmail.com>
2022-06-30 11:11:35 -0400

    atuzalizacao

--- jogo.py
+++ jogo.py
@@ -2,7 +2,7 @@ import pygame
 import random
 from enum import Enum
 from collections import namedtuple
-import numpy as np
+import numpy as np                  #pacote NumPy para cálculos numéricos
 
 pygame.init()
 font = pygame.font.Font('games-italic.ttf',25)
