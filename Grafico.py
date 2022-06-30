ec0588667a9a83504c4a357ac69d61bbd1e9f33a
Gabriel Rebouças <gabrielrbsr@gmail.com>
2022-06-30 11:13:10 -0400

    a

--- Grafico.py
+++ Grafico.py
@@ -1,26 +1,27 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt         
 from IPython import display
-import numpy
-import matplotlib
+import numpy          #pacote NumPy para cálculos numéricos
+import matplotlib  # usado com o NumPy para criação de gráficos e visualizações de dados
+
 from matplotlib import pyplot
 
-plt.ion()
+plt.ion() # Matplotlib é uma biblioteca de visualização em Python para gráficos 2D de matrizes.
 
-plt.style.use('dark_background')
+plt.style.use('dark_background') # tema de gráfico dark melhor visualizacão
 
-plt.show()
+plt.show() # função no módulo pyplot da biblioteca matplotlib é usada para exibir todas as figuras
 
 def plot(pontos, media):
-    display.clear_output(wait=False)
-    display.display(plt.gcf())
-    plt.clf()
+    display.clear_output(wait=False) # armazena em buffer os últimos n eventos. Sempre que o buffer muda,pode limpar a saída da célula e imprimir o buffer novamente.
+    display.display(plt.gcf()) # permite que você obtenha uma referência à figura atual ao usar pyplot .
+    plt.clf() # são usados ​​para limpar a figura atual, usado antes de definir um gráfico para que os anteriores não interfiram nos próximos.
     plt.title('Treinamento')
     plt.xlabel('Quantidade de jogos')
     plt.ylabel('Pontuação')
     plt.plot(pontos)
     plt.plot(media)
-    plt.ylim(ymin=0)
+    plt.ylim(ymin=0) # limite de eixo y
     plt.text(len(pontos)-1, pontos[-1], str(pontos[-1]))
     plt.text(len(media)-1, media[-1], str(media[-1]))
     plt.show(block=False)
-    plt.pause(.1)
+    plt.pause(.1) #pausa para nao haver erros
